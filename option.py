287f8c087eade7ff1a3da257be689ec6274d7128
Pau1 <13715125+Shuwe1Yang@users.noreply.github.com>
2018-02-26 01:08:39 -0500

    Add files via upload

---
+++ option.py
@@ -0,0 +1,14 @@
+class Option : 
+    def __init__(self, K, T):
+        self.K = K
+        self.T = T
+    def payoff(self, underlying_path):
+        pass
+#Call Option class
+class CallOption(Option):
+    def payoff(self, underlying_path):
+        return max(underlying_path[-1] - self.K, 0)
+#Put Option class
+class PutOption(Option):
+    def payoff(self, underlying_path):
+        return max(self.K - underlying_path[-1], 0)
